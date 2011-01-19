ba09fe095301568561ee53acb5d76be1b011c38e
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-19 00:20:10 -0500

    Added licence and project files.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljss "0.1.0"
+  :description "Lightweight CSS generating library."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.6.0"]])
